84b1ae5e6cc3b79f359269389679261705fa7459
jerrylou <gunsluo@gmail.com>
2017-01-20 16:42:01 +0800

    modify utils test
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- utils_test.go
+++ utils_test.go
@@ -13,13 +13,6 @@ func test_newjson(filename, content string) error {
 	}
 	defer f.Close()
 
-	/*
-		_, err := f.Write([]byte(content))
-		if err != nil {
-			return err
-		}
-	*/
-
 	w := bufio.NewWriter(f)
 	_, err = w.WriteString(content)
 	if err != nil {
@@ -34,19 +27,25 @@ func test_newjson(filename, content string) error {
 	return nil
 }
 
-func test_rmjson(filename string) {
+func test_rmjson(filename string) error {
+	err := os.Remove(filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func test_before(t *testing.T) {
+func test_before(t *testing.T, filename string) {
 	var content = "  jerrylou  "
-	err := test_newjson(".test.json", content)
+	err := test_newjson(filename, content)
 	if err != nil {
 		t.Error("Prepare the data is Interrupted.")
 	}
 }
 
-func test_after(t *testing.T) {
-	err := os.Remove(".test.json")
+func test_after(t *testing.T, filename string) {
+	err := test_rmjson(filename)
 	if err != nil {
 		t.Error("Prepare the data is Interrupted.")
 	}
@@ -54,7 +53,7 @@ func test_after(t *testing.T) {
 
 func Test_ToString(t *testing.T) {
 
-	test_before(t)
+	test_before(t, ".test.json")
 
 	str, err := ToString(".test.json")
 	if err != nil {
@@ -66,22 +65,46 @@ func Test_ToString(t *testing.T) {
 		t.Error("ToString not expect vaule. expect:", expect, "act:", str)
 	}
 
-	test_after(t)
+	test_after(t, ".test.json")
+}
+
+func Test_ToString2(t *testing.T) {
+
+	test_before(t, ".test.json")
+
+	_, err := ToString(".test2.json")
+	if err == nil {
+		t.Error("ToString exec failed.")
+	}
+
+	test_after(t, ".test.json")
 }
 
 func Test_ToTrimString(t *testing.T) {
 
-	test_before(t)
+	test_before(t, ".test.json")
 
 	str, err := ToTrimString(".test.json")
 	if err != nil {
-		t.Error("ToString exec failed.", err)
+		t.Error("ToTrimString exec failed.", err)
 	}
 
 	var expect = "jerrylou"
 	if str != expect {
-		t.Error("ToString not expect vaule. expect:", expect, "act:", str)
+		t.Error("ToTrimString not expect vaule. expect:", expect, "act:", str)
 	}
 
-	test_after(t)
+	test_after(t, ".test.json")
+}
+
+func Test_ToTrimString2(t *testing.T) {
+
+	test_before(t, ".test.json")
+
+	_, err := ToTrimString(".test2.json")
+	if err == nil {
+		t.Error("ToTrimString exec failed.")
+	}
+
+	test_after(t, ".test.json")
 }
